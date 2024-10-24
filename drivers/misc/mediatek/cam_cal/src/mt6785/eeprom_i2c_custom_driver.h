8bcd104e7e4331908d3de1eb2c00d14c03304164
Notganesh <chimeports@gmail.com>
2024-10-24 10:25:32 +0000

    drivers/misc/mediatek/cam_cal: import OnePlus changes
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- drivers/misc/mediatek/cam_cal/src/mt6785/eeprom_i2c_custom_driver.h
+++ drivers/misc/mediatek/cam_cal/src/mt6785/eeprom_i2c_custom_driver.h
@@ -1,8 +1,8 @@
+
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
  * Copyright (c) 2019 MediaTek Inc.
- */
-
+*/
 #ifndef __EEPROM_I2C_CUSTOM_DRIVER_H
 #define __EEPROM_I2C_CUSTOM_DRIVER_H
 #include <linux/i2c.h>
@@ -16,12 +16,5 @@ unsigned int Custom_read_region(struct i2c_client *client,
 				unsigned int addr,
 				unsigned char *data,
 				unsigned int size);
-unsigned int Hi556_read_region(struct i2c_client *client,
-				unsigned int addr,
-				unsigned char *data,
-				unsigned int size);
-unsigned int Hi846_read_region(struct i2c_client *client,
-				unsigned int addr,
-				unsigned char *data,
-				unsigned int size);
+
 #endif				/* __CAM_CAL_LIST_H */
--- drivers/misc/mediatek/cam_cal/src/mt6785/eeprom_i2c_dev.c
+++ drivers/misc/mediatek/cam_cal/src/mt6785/eeprom_i2c_dev.c
@@ -1,14 +1,15 @@
-// SPDX-License-Identifier: GPL-2.0
+/* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (c) 2020 MediaTek Inc.
- */
+ * Copyright (c) 2019 MediaTek Inc.
+*/
+
 #include "eeprom_i2c_dev.h"
 
 static enum EEPROM_I2C_DEV_IDX gi2c_dev_sel[IMGSENSOR_SENSOR_IDX_MAX_NUM] = {
 	I2C_DEV_IDX_1, /* main */
 	I2C_DEV_IDX_2, /* sub */
-	I2C_DEV_IDX_2, /* main2 */
-	I2C_DEV_IDX_2, /* sub2 */
+	I2C_DEV_IDX_3, /* main2 */
+	I2C_DEV_IDX_1, /* sub2 */
 	I2C_DEV_IDX_3, /* main3 */
 };
 
@@ -22,7 +23,6 @@ enum EEPROM_I2C_DEV_IDX get_i2c_dev_sel(enum IMGSENSOR_SENSOR_IDX idx)
 int gi2c_dev_timing[I2C_DEV_IDX_MAX] = {
 	100, /* dev1, 100k */
 	100, /* dev2, 100k */
-	100, /* dev3, 100k */
 	100, /* dev3, 100k */
 };
 
