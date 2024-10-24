6fda33fd01fcebc385d65a2bfad8444050b20b69
Notganesh <chimeports@gmail.com>
2024-10-24 10:25:53 +0000

    drivers/misc/mediatek/performance: import OnePlus changes
    Signed-off-by: Notganesh <chimeports@gmail.com>

--- drivers/misc/mediatek/performance/fpsgo_v3/fbt/platform/mt6873/fbt_cpu_platform.c
+++ drivers/misc/mediatek/performance/fpsgo_v3/fbt/platform/mt6873/fbt_cpu_platform.c
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright (C) 2020 MediaTek Inc.
+ * Copyright (c) 2019 MediaTek Inc.
  */
 
 #include "eas_ctrl.h"
@@ -8,15 +8,14 @@
 #include <fpsgo_common.h>
 #include <linux/pm_qos.h>
 #include <linux/cpumask.h>
-#include <linux/soc/mediatek/mtk-pm-qos.h>
 
-static struct mtk_pm_qos_request dram_req;
+static struct pm_qos_request dram_req;
 static struct cpumask mask[FPSGO_PREFER_TOTAL];
 static int mask_done;
 
 void fbt_notify_CM_limit(int reach_limit)
 {
-#ifdef CONFIG_MTK_CM_MGR_LEGACY
+#ifdef CONFIG_MTK_CM_MGR
 	cm_mgr_perf_set_status(reach_limit);
 #endif
 	fpsgo_systrace_c_fbt_gm(-100, 0, reach_limit, "notify_cm");
@@ -25,30 +24,30 @@ void fbt_notify_CM_limit(int reach_limit)
 void fbt_reg_dram_request(int reg)
 {
 	if (reg) {
-		if (!mtk_pm_qos_request_active(&dram_req))
-			mtk_pm_qos_add_request(&dram_req, MTK_PM_QOS_DDR_OPP,
-					MTK_PM_QOS_DDR_OPP_DEFAULT_VALUE);
+		if (!pm_qos_request_active(&dram_req))
+			pm_qos_add_request(&dram_req, PM_QOS_DDR_OPP,
+					PM_QOS_DDR_OPP_DEFAULT_VALUE);
 	} else {
-		if (mtk_pm_qos_request_active(&dram_req))
-			mtk_pm_qos_remove_request(&dram_req);
+		if (pm_qos_request_active(&dram_req))
+			pm_qos_remove_request(&dram_req);
 	}
 }
 
 void fbt_boost_dram(int boost)
 {
-	if (!mtk_pm_qos_request_active(&dram_req)) {
+	if (!pm_qos_request_active(&dram_req)) {
 		fbt_reg_dram_request(1);
-		if (!mtk_pm_qos_request_active(&dram_req)) {
+		if (!pm_qos_request_active(&dram_req)) {
 			fpsgo_systrace_c_fbt_gm(-100, 0, -1, "dram_boost");
 			return;
 		}
 	}
 
 	if (boost)
-		mtk_pm_qos_update_request(&dram_req, 0);
+		pm_qos_update_request(&dram_req, 0);
 	else
-		mtk_pm_qos_update_request(&dram_req,
-				MTK_PM_QOS_DDR_OPP_DEFAULT_VALUE);
+		pm_qos_update_request(&dram_req,
+				PM_QOS_DDR_OPP_DEFAULT_VALUE);
 
 	fpsgo_systrace_c_fbt_gm(-100, 0, boost, "dram_boost");
 }
@@ -72,16 +71,12 @@ void fbt_clear_boost_value(void)
 void fbt_set_per_task_min_cap(int pid, unsigned int base_blc)
 {
 	int ret = -1;
-	unsigned int base_blc_1024;
 
 	if (!pid)
 		return;
 
-	base_blc_1024 = (base_blc << 10) / 100U;
-	base_blc_1024 = clamp(base_blc_1024, 1U, 1024U);
-
 #ifdef CONFIG_UCLAMP_TASK
-	ret = set_task_util_min(pid, base_blc_1024);
+	ret = set_task_util_min_pct(pid, base_blc);
 #endif
 	if (ret != 0) {
 		fpsgo_systrace_c_fbt(pid, 0, ret, "uclamp fail");
@@ -96,12 +91,16 @@ static int generate_cpu_mask(unsigned int prefer_type, struct cpumask *cpu_mask)
 {
 	if (prefer_type == FPSGO_PREFER_LITTLE) {
 		cpumask_setall(cpu_mask);
+		cpumask_clear_cpu(4, cpu_mask);
+		cpumask_clear_cpu(5, cpu_mask);
 		cpumask_clear_cpu(6, cpu_mask);
 		cpumask_clear_cpu(7, cpu_mask);
 	} else if (prefer_type == FPSGO_PREFER_NONE)
 		cpumask_setall(cpu_mask);
 	else if (prefer_type == FPSGO_PREFER_BIG) {
 		cpumask_clear(cpu_mask);
+		cpumask_set_cpu(4, cpu_mask);
+		cpumask_set_cpu(5, cpu_mask);
 		cpumask_set_cpu(6, cpu_mask);
 		cpumask_set_cpu(7, cpu_mask);
 	} else
@@ -134,7 +133,6 @@ void fbt_set_affinity(pid_t pid, unsigned int prefer_type)
 
 void fbt_set_cpu_prefer(int pid, unsigned int prefer_type)
 {
-#if defined(CONFIG_MTK_SCHED_CPU_PREFER)
 	long ret;
 
 	if (!pid)
@@ -142,7 +140,6 @@ void fbt_set_cpu_prefer(int pid, unsigned int prefer_type)
 
 	ret = sched_set_cpuprefer(pid, prefer_type);
 	fpsgo_systrace_c_fbt(pid, 0, prefer_type, "set_cpuprefer");
-#endif
 }
 
 int fbt_get_L_min_ceiling(void)
